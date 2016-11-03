cc76c6388c2d6616a98550ba8bd87907b45b64ef
santosh s c <san.461@gmail.com>
2016-11-03 14:12:35 +0530

    prints ascill codes with respect to charecter

---
+++ PrintAsilCodeAssignment4.java
@@ -0,0 +1,14 @@
+
+public class PrintAsilCodeAssignment4 {
+
+	public static void main(String[] args) {
+		int i;
+		for (i=65;i<90;i++)
+		{
+		int code = i;
+		char asci = (char) code;
+		System.out.println(asci + "-" +i);
+		}
+	}
+
+}
